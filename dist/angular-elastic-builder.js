7c12de26218724aeca5a42ef966e8f9df6a9f0d0
Kassim Belghait <kassim@sirateck.com>
2017-04-05 12:32:02 +0200

    Update dist

--- dist/angular-elastic-builder.js
+++ dist/angular-elastic-builder.js
@@ -595,6 +595,12 @@
       , type = typeMap[key] || 'item'
       , obj = getFilterTemplate(type);
 
+      //Little hack to process match key as term
+      var fieldNameTmp = Object.keys(group[key])[0];
+      if(key === 'match' && !group[key][fieldNameTmp].minimum_should_match){
+        key = 'term';
+      }
+
     switch (key) {
       case 'bool':
           //Transform fo array
@@ -644,7 +650,6 @@
             var nameSplitted = originalFieldName.split('.');
             fieldName = nameSplitted[0];
             slug_name_value = nameSplitted[1];
-            //console.log('slug_name_value',slug_name_value);
           }
 
           var fieldData = fieldMap.filter(function(f){
@@ -659,7 +664,6 @@
 
             if(obj.field.object){
 
-
               var fieldKey = obj.field.fieldKey || 'name';
               var fieldValue = obj.field.fieldValue || 'value';
               var fieldKeyPath = fieldName + '.' + fieldKey;
@@ -673,7 +677,6 @@
               });
 
             }
-
 
              obj.matchingPercent = parseInt(group[key][originalFieldName].minimum_should_match.slice(0, -1));
              obj.operator = group[key][originalFieldName].operator;
@@ -697,6 +700,9 @@
         break;
       case 'term':
       case 'terms':
+
+      //little hack :-(
+      if(!group[key]) key = 'match';
 
 
         var originalFieldName = Object.keys(group[key])[0];
@@ -885,7 +891,7 @@
 
 
             if(fieldData.nested){
-              obj.term = {};
+              obj.match = {};
 
              //var nestedPath = fieldData.nestedPath || fieldName;
               var fieldKey = fieldData.fieldKey || 'name';
@@ -894,17 +900,17 @@
               var fieldValuePath = fieldName + '.' + fieldValue
               if(group.value)
               {
-                obj.term[fieldValuePath]  = group.value;
+                obj.match[fieldValuePath]  = group.value;
               }
               else{
-                obj.term[fieldKeyPath]  = group.valueKey;
+                obj.match[fieldKeyPath]  = group.valueKey;
               }
 
 
             }
             else{
-              obj.term = {};
-              obj.term[fieldName] = group.value;
+              obj.match = {};
+              obj.match[fieldName] = group.value;
             }
 
             break;
